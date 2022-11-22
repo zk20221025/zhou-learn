8196c721a5dbafe646dc847ee90dc75a26874f1d
zhou <1111>
2022-11-22 14:45:01 +0800

    upload Constant

--- Constant.sol
+++ Constant.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 contract Constant {
-    uint256 constant CONSTANT_NUM = 10;
-    string constant CONSTANT_STRING = "0xAA";
-    bytes constant CONSTANT_BYTES = "WTF";
-    address constant CONSTANT_ADDRESS = 0x0000000000000000000000000000000000000000;
+    uint256 public constant CONSTANT_NUM = 10;
+    string public constant CONSTANT_STRING = "0xAA";
+    bytes public constant CONSTANT_BYTES = "WTF";
+    address public constant CONSTANT_ADDRESS = 0x0000000000000000000000000000000000000000;
     uint256 public immutable IMMUTABLE_NuM = 999999999;
     address public immutable IMMUTABLE_ADDRESS;
     uint256 public immutable IMMUTABLE_BLOCK;

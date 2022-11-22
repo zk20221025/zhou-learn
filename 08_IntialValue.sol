47ad09984f01d70d6a407d79e20bed0f6733198c
zhou <1111>
2022-11-22 20:54:58 +0800

    upload inheritance/change name

---
+++ 08_IntialValue.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+contract IntialValue {
+    bool public _bool;//false
+    string public _student;//""
+    int public _int; //0
+    uint public _uint; // 0
+    address public _address;//0x0000000000000000000000000000000000000000
+    enum ActionSet {Buy,Hold,Sell}
+    ActionSet public _enum;
+    function fi() internal{}
+    function fiCall() external{
+        fi();
+    }
+    function fe() external{}
+    uint[8] public _staticArray;//[0,0,0,0,0,0,0,0,]
+    uint[] public _dynamicArrayl;//[]
+    mapping(uint => address) public _mapping;
+    struct Student{
+       uint256 id;
+       uint256 score;
+    }
+    Student public student;
+    bool public _bool2 = true;
+    function d() external {
+        delete _bool2;
+    }
+}
---
+++ 11_Owner.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Owner {
+    address public owner;
+    constructor() {
+        owner = msg.sender;
+    }
+    modifier onlyOwner {
+        require (msg.sender == owner);
+        _;
+    }
+    function changeOwner(address _newOwner) external onlyOwner {
+        owner = _newOwner;
+    }
+}

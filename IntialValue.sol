344e311475e00dc9698b60c3bc70e58c2b691d7d
zhou <1111>
2022-11-22 13:31:40 +0800

    修改

--- IntialValue.sol
+++ IntialValue.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+contract IntialValue {
+    bool public _bool;//false
+    string public _student;//""
+    int public _int; //0
+    uint public _uint; // 0
+    address public _address;//0x0000000000000000000000000000000000000000
+    enum ActionSet {Buy,Hold,Sell}
+    ActionSet public _enum;
+    function fi() internal{}
+    function fe() external{}
+}

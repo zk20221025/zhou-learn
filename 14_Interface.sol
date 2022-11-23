d0fe9eb5ab6a51acf169c06b8c1cf3a7e6527372
zhou <1111>
2022-11-23 15:29:16 +0800

    update Interface

--- 14_Interface.sol
+++ 14_Interface.sol
@@ -5,5 +5,10 @@ abstract contract InsertionSort {
 }
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 interface IERC721 is IERC165 {
-
+    event Transfer(address indexed from , address  indexed to , uint256 indexed TokenId);
+    event Approval(address indexed owner , address indexed approved , uint256 indexed TokenId);
+    event ApprovalForAll(address indexed owner , address indexed operator , bool approved);
+    function balanceOf(address owner) external view returns(uint256 balance);
+    function ownerOf(uint256 TokenId) external view returns(address owner);
+    
 }

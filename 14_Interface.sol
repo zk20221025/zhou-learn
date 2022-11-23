f5d4c77e4043ac8603bf13d15dc237e5f27c9b61
zhou <1111>
2022-11-23 15:49:07 +0800

    update Interface

--- 14_Interface.sol
+++ 14_Interface.sol
@@ -10,5 +10,11 @@ interface IERC721 is IERC165 {
     event ApprovalForAll(address indexed owner , address indexed operator , bool approved);
     function balanceOf(address owner) external view returns(uint256 balance);
     function ownerOf(uint256 TokenId) external view returns(address owner);
-    
+    function safeTransferFrom(address from , address to , uint256 TokenId) external;
+    function transferFrom(address from , address to , uint256 TokenId) external;
+    function approve(address to , uint256 TokenId) external;
+    function getApproved(uint256 TokenId) external view returns(address operator);
+    function setApprovalForAll(address operator , bool _approve) external;
+    function isApprovalForAll(address owner , address operator) external view returns(bool);
+    function safeTransferFrom(address form , address to , uint256 TokenId , bytes calldata data) external;
 }

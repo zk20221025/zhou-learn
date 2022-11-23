c95e844db42e94da03c308aba43966fe8aac4760
zhou <1111>
2022-11-23 13:11:23 +0800

    update inheritance

--- 13_Inheritance.sol
+++ 13_Inheritance.sol
@@ -1,2 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+contract Yeye {
+    event Log(string msg);
+    function hip() public virtual {
+        emit Log ("Yeye");
+    }
+    function pop() public virtual {
+        emit Log ("Yeye");
+    }
+    function yeye() public virtual {
+        emit Log ("Yeye");
+    }
+}
+contract Baba is Yeye {
+    function hip() public virtual override {
+        emit Log ("Baba");
+    }
+    function pop() public virtual override {
+        emit Log ("Baba");
+    }
+    function baba() public virtual {
+        emit Log ("Baba");
+    }
+}

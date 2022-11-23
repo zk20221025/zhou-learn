5c2c3c8be48aeef19f4ed8b5566257d9638ae5d9
zhou <1111>
2022-11-23 14:54:16 +0800

    update Interface

--- 14_Interface.sol
+++ 14_Interface.sol
@@ -1,2 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+abstract contract InsertionSort {
+    function insertionSort(uint[] memory a) public pure virtual returns(uint[] memory);
+}
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+interface IERC721 is IERC165 {
+
+}

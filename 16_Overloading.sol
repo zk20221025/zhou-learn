263f292a575a94f9ab037c33bcfd386ae04af3c3
zhou <1111>
2022-11-24 19:31:45 +0800

    update Overloading

--- 16_Overloading.sol
+++ 16_Overloading.sol
@@ -1,2 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+contract Overload {
+    function saySomething() public pure returns(string memory) {
+        return ("Nothing");
+    }
+
+}

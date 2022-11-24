920c13c3bee1d4164673b5484c5d8186728b0336
zhou <1111>
2022-11-24 19:53:24 +0800

    update Overloading

--- 16_Overloading.sol
+++ 16_Overloading.sol
@@ -7,4 +7,10 @@ contract Overload {
     function saySomething(string memory something) public pure returns (string memory) {
         return (something);
     }
+    function f(uint8 _in) public pure returns (uint8 out) {
+        out = _in ;
+    }
+    function f(uint256 _in) public pure returns (uint256 out) {
+        out = _in ;
+    }
 }

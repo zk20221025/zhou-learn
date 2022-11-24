483671af1480664318698560afb49679c95e69bf
zhou <1111>
2022-11-24 20:20:21 +0800

    update Library

--- 17_Library.sol
+++ 17_Library.sol
@@ -1,5 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 contract Strings {
-    
+    bytes16 private constant _HEX_SYMBOLS = "0123456789abcdef";
+    function tpString(uint256 Value) public pure returns (string memory) {
+        if (Value == 0) {
+            return "0";
+        }
+        uint256 temp = Value;
+        uint256 digits;
+        while (temp != 0){
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (Value != 0){
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(Value / 10)));
+        }
+        return string (buffer);
+    }
 }

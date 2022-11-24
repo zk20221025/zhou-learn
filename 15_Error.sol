c480b11a341805c9ea204c63e971344f4a7c9340
zhou <1111>
2022-11-24 13:18:55 +0800

    upload

--- 15_Error.sol
+++ 15_Error.sol
@@ -1,2 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
+error TransferNotOwner();
+
+contract Errors {
+    mapping(uint256 => address) private _owner;
+
+    function transferOwner1(uint256 TokenId , address _newOwner) public {
+        if (_owners[TokenId] != msg.sender) {
+            revert transferNotOwner() ;
+        }
+        _owners[TokenId] = newOwner ;
+    }
+}

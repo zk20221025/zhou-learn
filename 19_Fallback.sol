d83d41ca0e85d7a2f5130324cdf2eb4b70ac52b3
zhou <1111>
2022-11-25 20:35:47 +0800

    update

--- 19_Fallback.sol
+++ 19_Fallback.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 contract Fallback {
-    event Received (address sender , uint256 value);
+    event Received (address Sender , uint256 Value);
+    event fallbackCalled (address Sender , uint256 Value , bytes Data);
     receive() external payable {
         emit Received(msg.sender , msg.value);
     }
+    fallback() external payable {
+        emit fallbackCalled(msg.sender , msg.value , msg.data);
+    }
+
 }

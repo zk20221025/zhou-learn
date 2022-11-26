fcf792ab075061c0e45b5963b91bfefbd4ad7c1c
zhou <1111>
2022-11-26 14:51:06 +0800

    update

--- 20_SendETH.sol
+++ 20_SendETH.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+contract ReceiveETH {
+    event Log (uint amount , uint gas);
+
+    receive() external payable {
+        emit Log (msg.value , gasleft());
+    }
+
+    function getBalance() view public returns(uint) {
+        return address(this).balance;
+    }
+}

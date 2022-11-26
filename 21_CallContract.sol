73550eb73e49ee44fb35dc6eeb191f7d0767bf4d
zhou <1111>
2022-11-26 20:41:10 +0800

    更新 目标合约

--- 21_CallContract.sol
+++ 21_CallContract.sol
@@ -2,13 +2,20 @@
 pragma solidity ^0.8.17;
 contract OtherContract {
     uint256 private _x = 0;
-    event log(uint amount , uint gas);
+    event Log(uint amount , uint gas);
 
     function getBalance() view public returns(uint) {
         return address(this).balance;
     }
 
     function setX(uint256 x) external payable {
-        
+        _x = x ;
+        if (msg.value > 0) {
+            emit Log(msg.value , gasleft());
+        }
+    }
+
+    function getX() external view returns(uint x) {
+        x = _x ;
     }
 }

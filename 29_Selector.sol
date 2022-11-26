ba6855c4cbf7c168c1e2d6168db6bdcfaa09d57c
zhou <1111>
2022-11-26 13:53:27 +0800

    update

--- 29_Selector.sol
+++ 29_Selector.sol
@@ -1,2 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+contract Selector {
+
+    event Log(bytes data);
+
+    function mint(address to) external {
+        emit Log(msg.data);
+    }
+
+}

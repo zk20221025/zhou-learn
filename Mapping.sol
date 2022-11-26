6179576b3f6edd9ba727c8e818cc9c83467bb8c0
zhou <1111>
2022-11-26 20:16:24 +0800

    删除旧版本

--- Mapping.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-contract Mapping {
-    mapping(uint => address) public inToaddress;
-    mapping(address => address) public swapPair;
-    function writeMap(uint _Key, address _Value) public {
-        inToaddress[_Key] = _Value;
-    }
-}

2d83782ff61245c574f2a5143c30d48355265ea0
zhou <1111>
2022-11-26 13:17:29 +0800

    upload SendETH

--- 19_Fallback.sol
+++ 19_Fallback.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
 contract Fallback {
     event Received (address Sender , uint256 Value);
     event fallbackCalled (address Sender , uint256 Value , bytes Data);

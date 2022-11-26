dfa782d2e0875a671751a4dad8ab6e0e07769f83
zhou <1111>
2022-11-26 21:00:32 +0800

    更新 调用合约

--- 21_CallContract.sol
+++ 21_CallContract.sol
@@ -19,3 +19,22 @@ contract OtherContract {
         x = _x ;
     }
 }
+
+contract CallContract {
+    function callSetX(address _Address , uint256 x) external {
+        OtherContract(_Address).setX(x);
+    }
+
+    function callGetX(OtherContract _Address) external view returns(uint x) {
+        x = _Address.getX();
+    }
+
+    function callGetX2(address _Address) external view returns(uint x){
+        OtherContract oc = OtherContract(_Address);
+        x = oc.getX();
+    }
+
+    function setXTransferETH(address otherContract, uint256 x) payable external{
+        OtherContract(otherContract).setX{value: msg.value}(x);
+    }
+}

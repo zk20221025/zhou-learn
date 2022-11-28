31431bdc76280f3a5747dd02314bffd14ca0be69
zhou <1111>
2022-11-28 19:51:37 +0800

    更新 目标合约

---
+++ 22_Call.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract OtherContract {
+    uint256 private _x = 0 ;
+    event Log(uint amount , uint gas);
+    
+    fallback() external payable {}
+
+    function getBalance() view public returns(uint) {
+        return address(this).balance;
+    }
+
+    function setX(uint256 x) external payable {
+        _x = x ;
+        if (msg.value > 0) {
+            emit Log(msg.value , gasleft());
+        }
+    }
+
+    function getX() external view returns(uint x) {
+        x = _x ;
+    }
+}

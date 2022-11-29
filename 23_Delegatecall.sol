a939932b4dad13049ea8679f0aa9c801f5849ea7
zhou <1111>
2022-11-29 15:21:03 +0800

    上传合约C

--- 23_Delegatecall.sol
+++ 23_Delegatecall.sol
@@ -1,2 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
+contract C {
+    uint public num;
+    address public sender;
+
+    function setVars(uint _num) public payable {
+        num = _num;
+        sender = msg.sender;
+    }
+}

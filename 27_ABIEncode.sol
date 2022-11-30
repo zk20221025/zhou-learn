b8ce174a8004c67b83c760cc2580a79ad72961d3
zhou <1111>
2022-11-30 19:43:44 +0800

    更新 abi.encode

--- 27_ABIEncode.sol
+++ 27_ABIEncode.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.8.4;
 contract ABIEncode {
 
+    uint x = 10;
+    address addr = 0x7A58c0Be72BE218B41C608b7Fe7C5bB630736C71;
+    string name = "0xAA";
+    uint[2] array = [5,6];
+
+    function encode() public view returns (bytes memory result) {
+        result = abi.encode(x , addr , name , array);
+    }
+
     
 }

d0ea9e8e285f9b39d3440f9ef68ce6329ab32094
zhou <1111>
2022-11-30 20:17:40 +0800

    更新 abi

--- 27_ABIEncode.sol
+++ 27_ABIEncode.sol
@@ -11,5 +11,19 @@ contract ABIEncode {
         result = abi.encode(x , addr , name , array);
     }
 
-    
+    function encodePacked() public view returns (bytes memory result) {
+        result = abi.encodePacked(x , addr , name , array);
+    }
+
+    function encodeWithSignature() public view returns (bytes memory result) {
+        result = abi.encodeWithSignature("foo(uint256 , address , string , uint256[2])" , x , addr , name , array);
+    }
+
+    function encodeWithSelector() public view returns (bytes memory result) {
+        result = abi.encodeWithSelector(bytes4(keccak256("foo(uint256 , address , string , uint256[2])")), x , addr , name , array);
+    } 
+
+    function decode(bytes memory data) public pure returns(uint dx , address daddr , string memory dname , uint[2] memory darray) {
+        (dx , daddr , dname , darray) = abi.decode(data , (uint , address , string , uint[2]));
+    }
 }

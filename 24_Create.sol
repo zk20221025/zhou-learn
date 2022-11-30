06251c10b3a29875a3e0905cb0e9139c94c7f69e
zhou <1111>
2022-11-30 13:39:04 +0800

    更新 工厂合约

--- 24_Create.sol
+++ 24_Create.sol
@@ -16,3 +16,17 @@ contract Pair {
     token1 = _token1;        
     }
 }
+
+contract PairFactory {
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    function createPair(address tokenA , address tokenB) external returns (address pairAddr) {
+        Pair pair = new Pair();
+        pair.initialize(tokenA , tokenB);
+        pairAddr = address(pair);
+        allPairs.push(pairAddr);
+        getPair[tokenA][tokenB] = pairAddr;
+        getPair[tokenB][tokenA] = pairAddr;
+    }
+}

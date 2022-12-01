6c1edb8e03347a098cf554654411d1450026207d
zhou <1111>
2022-12-01 20:51:08 +0800

    更新 Faucet

--- 32_Faucet.sol
+++ 32_Faucet.sol
@@ -5,5 +5,80 @@ import "./31_IERC20.sol";
 
 contract ERC20 is IERC20 {
 
+    mapping(address => uint256) public override balanceOf;
+
+    mapping(address => mapping(address => uint256)) public override allowance;
+
+    uint256 public override totalSupply;
+
+    string public name;
+    string public symbol;
     
+    uint8 public decimals = 18;
+
+    constructor(string memory name_ , string memory symbol_) {
+        name = name_;
+        symbol = symbol_;
+    } 
+
+    function transfer(address recipient , uint amount) external override returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender , recipient , amount);
+        return true;
+    }
+
+    function approve(address spender , uint amount) external override returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender , amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external override returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender , recipient , amount);
+        return true;
+    }
+
+    function mint(uint amount) external {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0) , msg.sender , amount);
+    }
+
+    function burn(uint amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender , address(0) , amount);
+    }
+}
+
+contract Faucet {
+
+    uint256 public amountAllowed = 100;
+    address public tokenContract;
+    mapping(address => bool) public requetedAddress;
+
+    event SendToken(address indexed Receiver , uint256 indexed Amount);
+
+    constructor(address _tokenContract) {
+        tokenContract = _tokenContract;
+    }
+
+    function requestTokens() external {
+        require(requetedAddress[msg.sender] == false , "Can't Request Multiple Times!");
+        IERC20 token = IERC20(tokenContract);
+        require(token.balanceOf(address(this)) >= amountAllowed , "Faucet Empty!");
+
+        token.transfer(msg.sender , amountAllowed);
+        requetedAddress[msg.sender] = true;
+
+        emit SendToken(msg.sender , amountAllowed);
+    }
 }

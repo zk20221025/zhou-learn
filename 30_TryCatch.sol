df9e23c8662e6eeb3486bcf876b43f49198d1a78
zhou <1111>
2022-12-01 13:23:55 +0800

    更新 TryCatch

--- 30_TryCatch.sol
+++ 30_TryCatch.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.4;
 
 contract OnlyEven {
+
     constructor(uint a) {
         require(a != 0 , "invalid number");
         assert(a != 1);
@@ -10,5 +11,27 @@ contract OnlyEven {
     function onlyEven(uint256 b) external pure returns(bool success) {
         require(b % 2 == 0 , "Ups! Reverting");
         success = true;
+    }
+}
+
+contract TryCatch {
+
+    event SuccessEvent();
+    event CatchEvent(string message);
+    event CatchByte(bytes data);
+
+    OnlyEven even;
+
+    constructor() {
+        even = new OnlyEven(2);
+    }
+
+    function execute(uint amount) external returns (bool success) {
+        try even.onlyEven(amount) returns(bool _success) {
+            emit SuccessEvent();
+            return _success;
+        } catch Error(string memory reason) {
+            emit CatchEvent(reason);
+        }
     }
 }

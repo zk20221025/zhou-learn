c5b27d09f73741a5a2ca5a66403cfec3829890f1
zhou <1111>
2022-12-01 13:14:07 +0800

    上传 TryCatch

---
+++ 30_TryCatch.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract OnlyEven {
+    constructor(uint a) {
+        require(a != 0 , "invalid number");
+        assert(a != 1);
+    }
+
+    function onlyEven(uint256 b) external pure returns(bool success) {
+        require(b % 2 == 0 , "Ups! Reverting");
+        success = true;
+    }
+}

250ff1e0bf54ee09946a3d4a02a2eeb9ede933fd
zhou <1111>
2022-12-01 16:42:22 +0800

    更新 ERC20

--- 31_ERC20.sol
+++ 31_ERC20.sol
@@ -16,5 +16,40 @@ contract ERC20 is IERC20 {
 
     uint8 public decimals = 18;
 
-    
+    function transfer(address recipient , uint amount) external override returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender , recipient , amount);
+        return true;
+    }
+
+    function approve(address spender , uint amount) external override returns (bool) {
+        allowance[msg.sender][spender] = amount ;
+        emit Approval(msg.sender , spender , amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external override returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender , recipient , amount);
+        return true;
+    }
+
+    function mint(uint amount) external {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0) , msg.sender , amount);
+    }
+
+    function burn(uint amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender , address(0) , amount);
+    }
 }

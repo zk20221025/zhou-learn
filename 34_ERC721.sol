e8725a3cffa7177e649c4ced6939720360eba475
zhou <1111>
2022-12-02 21:01:53 +0800

    更新 ERC721主合约

--- 34_ERC721.sol
+++ 34_ERC721.sol
@@ -66,13 +66,15 @@ contract ERC721 is IERC721 , IERC721Metadata {
         emit Approval(owner , to , tokenId);
     }
 
-    function approve(address to , uint tokenId) external override {
+    function approve(address to, uint tokenId) external override {
         address owner = _owners[tokenId];
         require(
             msg.sender == owner || _operatorApprovals[owner][msg.sender],
             "not owner nor approved for all"
         );
+        _approve(owner, to, tokenId);
     }
+
 
     function _isApprovedOrOwner(
         address owner,

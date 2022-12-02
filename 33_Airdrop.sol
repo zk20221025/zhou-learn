813608b514ac6d3356312b183fd59e59a126e794
zhou <1111>
2022-12-02 15:49:31 +0800

    更新 Airdrop

--- 33_Airdrop.sol
+++ 33_Airdrop.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "31_ERC20.sol";
+import "31_IERC20.sol";
 
 contract Airdrop {
 
@@ -33,5 +33,60 @@ contract Airdrop {
         for(uint256 i = 0; i < _addresses.length; i++) {
             _addresses[i].transfer(_amounts[i]);
         }
+    }
+}
+
+contract ERC20 is IERC20 {
+    mapping(address => uint256) public override balanceOf;
+
+    mapping(address => mapping(address => uint256)) public override allowance;
+
+    uint256 public override totalSupply;
+
+    string public name;   
+    string public symbol;
+    
+    uint8 public decimals = 18;
+
+    constructor(string memory name_ , string memory symbol_) {
+        name = name_;
+        symbol = symbol_;
+    }
+
+    function transfer(address recipient, uint amount) external override returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint amount) external override returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external override returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function mint(uint amount) external {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
     }
 }

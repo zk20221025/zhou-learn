b538382e99f02d929d4c542986597db54589c04a
zhou <1111>
2022-12-08 13:27:27 +0800

    更新 MerkleTree合约

--- 36_MerkleTree.sol
+++ 36_MerkleTree.sol
@@ -35,5 +35,21 @@ contract MerkleTree is ERC721 {
         root = merkleroot;
     }
 
-    function
+    function mint(address account , uint256 tokenId , bytes32[] calldata proof) external {
+        require(_verify(_leaf(account), proof), "Invalid merkle proof");
+        require(!mintedAddress[account], "Already minted!");
+
+        mintedAddress[account] = true;
+        _mint(account , tokenId); 
+    }
+
+    function _leaf(address account) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(account));
+    }
+
+    function _verify(bytes32 leaf , bytes32[] memory proof)
+    internal view returns (bool) 
+    {
+        return MerkleProof.verify(proof , root , leaf);
+    }
 }

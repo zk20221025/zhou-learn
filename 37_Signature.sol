ca279f30b6c52f4b048001d3d0589d44c610bc19
zhou <1111>
2022-12-08 16:07:04 +0800

    更新 SignatureNFT 合约

--- 37_Signature.sol
+++ 37_Signature.sol
@@ -25,3 +25,32 @@ library ECDSA {
         return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32" , hash));
     }
 }
+
+contract SignatureNFT is ERC721 {
+    address immutable public signer;
+    mapping(address => bool) public mintedAddress;
+
+    constructor(string memory _name , string memory _symbol , address _signer)
+    ERC721(_name , _symbol)
+    {
+        signer = _signer;
+    }
+
+    function mint(address _account , uint256 _tokenId , bytes memory _signature) external {
+        bytes32 _msgHash = getMessageHash(_account , _tokenId);
+        bytes32 _ethSignedMessageHash = ECDSA.toEthSignedMessageHash(_msgHash);
+        require(verify(_ethSignedMessageHash , _signature) , "Invalid signature");
+        require(!mintedAddress[_account] , "Already minted!");
+
+        mintedAddress[_account] = true;
+        _mint(_account , _tokenId);
+    }
+
+    function getMessageHash(address _account , uint256 _tokenId) public pure returns(bytes32) {
+        return keccak256(abi.encodePacked(_account , _tokenId));
+    }
+
+    function verify(bytes32 _msgHash , bytes memory _signature) public view returns (bool) {
+        return ECDSA.verify(_msgHash , _signature , signer);
+    }
+}

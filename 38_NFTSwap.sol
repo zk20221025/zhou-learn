aad99fd131b3474c8123dc80c537376e6b4a5d58
zhou <1111>
2022-12-08 20:20:50 +0800

    上传 NFTSwap合约

---
+++ 38_NFTSwap.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./34_IERC721.sol";
+import "./34_IERC721Receiver.sol";
+import "./34_WTFApe.sol";
+
+contract NFTSwap is IERC721Receiver {
+    
+}

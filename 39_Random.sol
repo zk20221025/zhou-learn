1fd2ede1f4b39bd46bb78dc2345a5b0904ea3cef
zhou <1111>
2022-12-09 17:01:57 +0800

    上传 Random合约

---
+++ 39_Random.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./ERC721.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
+contract RabdonNumber is ERC721 , VRFConsumerBase {
+    
+}

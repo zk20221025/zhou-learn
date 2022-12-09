55457ef5f835d2053ffbd4afd9fbc72a46383460
zhou <1111>
2022-12-09 19:44:33 +0800

    更新 Random合约

--- 39_Random.sol
+++ 39_Random.sol
@@ -5,5 +5,8 @@ import "./ERC721.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
 contract RabdonNumber is ERC721 , VRFConsumerBase {
-    
+    function getRandomOnchain() public view returns(uint256) {
+        bytes32 randomBytes = keccak256(abi.encodePacked(blockhash(block.number-1) , msg.sender , block.timestamp));
+        return uint256(randomBytes);
+    }
 }

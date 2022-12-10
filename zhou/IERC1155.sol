c3effda837db9ce70943876d0790ac10e2126a05
zhou <1111>
2022-12-10 15:34:11 +0800

    更新 IERC155

--- zhou/IERC1155.sol
+++ zhou/IERC1155.sol
@@ -2,3 +2,20 @@
 pragma solidity ^0.8.0;
 
 import "./34_IERC165.sol";
+
+interface IERC1155 is IERC165 {
+
+    event TransferSingle(address indexed operator , address indexed from , address indexed to , uint256 id , uint256 value);
+
+    event TransferBatch(
+        address indexed operator,
+        address indexed from,
+        address indexed to,
+        uint256[] id,
+        uint256[] values
+    );
+
+    event ApprovalForAll(address indexed account , address indexed operator , bool approved);
+
+    event URI(string value , uint256 indexed id);
+}

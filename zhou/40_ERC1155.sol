287bc8c72a2b1644955f626283f70e96e5e60be3
zhou <1111>
2022-12-10 17:07:56 +0800

    upload ERC1155合约

---
+++ zhou/40_ERC1155.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./IERC1155.sol";
+import "./IERC1155Receiver.sol";
+import "./IERC1155MetadataURI.sol";
+import "../34_Address.sol";
+import "../34_String.sol";
+import "./34_IERC165.sol";
+
+contract ERC1155 is IERC165 , IERC1155 , IERC1155MetadataURI {
+    
+}

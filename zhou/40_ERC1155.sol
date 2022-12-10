57c2b767cef22400836e171182463c9255ea43ff
zhou <1111>
2022-12-10 20:57:11 +0800

    update ERC1155合约

--- zhou/40_ERC1155.sol
+++ zhou/40_ERC1155.sol
@@ -79,4 +79,35 @@ contract ERC1155 is IERC165 , IERC1155 , IERC1155MetadataURI {
         _balances[id][to] += amount;
         emit TransferSingle(operator , from , to , id , amount , data);
     }
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public virtual override {
+        address operator = msg.sender;
+        require(
+            from == operator || isApprovedForAll(from , operator),
+            "ERC1155: caller is not token owner nor approved"
+        );
+        require(ids.length == amounts.length, "ERC1155: ids and amounts length mismatch");
+        require(to != address(0), "ERC1155: transfer to the zero address");
+
+            for (uint256 i = 0; i < ids.length; ++i) {
+            uint256 id = ids[i];
+            uint256 amount = amounts[i];
+
+            uint256 fromBalance = _balances[id][from];
+            require(fromBalance >= amount, "ERC1155: insufficient balance for transfer");
+            unchecked {
+                _balances[id][from] = fromBalance - amount;
+            }
+            _balances[id][to] += amount;
+        }
+
+        emit TransferBatch(operator, from, to, ids, amounts);
+        _doSafeBatchTransferAcceptanceCheck(operator, from, to, ids, amounts, data);
+    }
 }

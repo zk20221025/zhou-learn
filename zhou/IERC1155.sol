c5c637717f3476a780830e32ae23fad0b9d89423
zhou <1111>
2022-12-10 16:05:52 +0800

    更新 IERC1155

--- zhou/IERC1155.sol
+++ zhou/IERC1155.sol
@@ -18,4 +18,29 @@ interface IERC1155 is IERC165 {
     event ApprovalForAll(address indexed account , address indexed operator , bool approved);
 
     event URI(string value , uint256 indexed id);
+
+    function balanceOfBatch(address[] calldata accounts , uint256[] calldata ids)
+    external
+    view
+    returns (uint256[] memory);
+
+    function setApprovalForAll(address operator , bool approved) external;
+
+    function isApprovedForAll(address account , address operator) external view returns (bool);
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes calldata data
+    ) external;
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        bytes calldata data
+    ) external;
 }

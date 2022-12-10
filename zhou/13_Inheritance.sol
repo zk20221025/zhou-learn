ab79ba3d557011d9f22dc4138e764800719056b6
zhou <1111>
2022-12-10 14:50:53 +0800

    修改文件位置

---
+++ zhou/13_Inheritance.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+contract Yeye {
+    event Log(string msg);
+    function hip() public virtual {
+        emit Log ("Yeye");
+    }
+    function pop() public virtual {
+        emit Log ("Yeye");
+    }
+    function yeye() public virtual {
+        emit Log ("Yeye");
+    }
+}
+contract Baba is Yeye {
+    function hip() public virtual override {
+        emit Log ("Baba");
+    }
+    function pop() public virtual override {
+        emit Log ("Baba");
+    }
+    function baba() public virtual {
+        emit Log ("Baba");
+    }
+}
+contract Erzi is Yeye , Baba {
+    function hip() public virtual override (Yeye, Baba){
+        emit Log ("Erzi");
+    }
+    function pop() public virtual override (Yeye, Baba){
+        emit Log ("Erzi");
+    }
+    function callParent() public {
+        Yeye.pop();
+    }
+    function callParentSuper() public {
+        super.pop();
+    }
+}
+contract Base1 {
+    modifier exactDivideBy2And3(uint _a) virtual {
+        require(_a % 2 == 0 && _a % 3 == 0 );
+        _;
+    }
+}
+contract Identifier is Base1 {
+    function getExactDivideBy2And3(uint _dividend) public exactDivideBy2And3(_dividend) pure returns(uint, uint) {
+        return getExactDivideBy2And3WithoutModifier(_dividend);
+    } 
+    function getExactDivideBy2And3WithoutModifier(uint _dividend) public pure returns (uint, uint) {
+        uint div2 = _dividend / 2;
+        uint div3 = _dividend / 3;
+        return (div2, div3);
+    }
+    modifier exactDivideBy2And3(uint _a) override {
+        _;
+        require ( _a % 2 == 0 && _a % 3 == 0 );
+    }
+}
---
+++ zhou/20_SendETH.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+error SendFailed();
+error CallFailed();
+
+contract ReceiveETH {
+    event Log (uint amount , uint gas);
+
+    receive() external payable {
+        emit Log (msg.value , gasleft());
+    }
+
+    function getBalance() view public returns(uint) {
+        return address(this).balance;
+    }
+}
+
+contract SendETH {
+    constructor() payable {}
+    receive() external payable{}
+
+    function transferETH (address payable _to , uint256 amount) external payable {
+        _to.transfer(amount);
+    }
+
+    function sendETH (address payable _to , uint256 amount) external payable {
+        bool success = _to.send(amount);
+        if (!success) {
+            revert SendFailed();
+        }
+    }
+
+    function callETtH (address payable _to , uint256 amount) external payable {
+        (bool success,) = _to.call{value: amount}("");
+        if (!success) {
+            revert CallFailed();
+        }
+    }
+}

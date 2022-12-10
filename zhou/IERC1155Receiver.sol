add583faa60976ea1681934ad63d772d80219d69
zhou <1111>
2022-12-10 16:24:02 +0800

    upload IERC1155Receiver合约

---
+++ zhou/IERC1155Receiver.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./34_IERC165.sol";
+
+interface IERC1155Receiver is IERC165 {
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external returns (bytes4);
+
+    
+}

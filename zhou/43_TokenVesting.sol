13a5cb764985a93340e7bc8f44cc8a89cdb41957
zhou <1111>
2022-12-13 15:07:35 +0800

    upload TokenVesting合约

---
+++ zhou/43_TokenVesting.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../31_ERC20.sol";
+
+contract TokenVesting {
+    event ERC20Released(address indexed token , uint256 amount);
+    
+}

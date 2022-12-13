ed793a66f558974810fa292268f9af6a12d54910
zhou <1111>
2022-12-13 15:33:22 +0800

    upload TokenVesting合约(事件/映射/构造函数）

--- zhou/43_TokenVesting.sol
+++ zhou/43_TokenVesting.sol
@@ -5,5 +5,19 @@ import "../31_ERC20.sol";
 
 contract TokenVesting {
     event ERC20Released(address indexed token , uint256 amount);
-    
+
+    mapping(address => uint256) public erc20Released;
+    address public immutable beneficiary;
+    uint256 public immutable start;
+    uint256 public immutable duration;
+
+    constructor(
+        address beneficiaryAddress,
+        uint256 durationSeconds
+    ) {
+        require(beneficiaryAddress != address(0) , "VestingWallet: beneficiary is zero address");
+        beneficiary = beneficiaryAddress;
+        start = block.timestamp;
+        duration = durationSeconds;
+    }
 }

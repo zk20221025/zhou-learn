60d2ca37bf402934afcd8e16e0bd2abd6f9868cf
zhou <1111>
2022-12-13 13:34:45 +0800

    update PaymentSplit合约分账函数

--- zhou/42_PaymentSplit.sol
+++ zhou/42_PaymentSplit.sol
@@ -13,5 +13,25 @@ contract PaymentSplit {
     mapping(address => uint256) public released;
     address[] public payees;
 
-    
+    constructor(address[] memory _payees , uint256[] memory _shares) payable {
+        require(_payees.length == _shares.length , "PaymentSplitter: payees and shares length mismatch");
+        require(_payees.length > 0 , "PaymentSplitter: no payees");
+        for (uint256 i = 0; i < _payees.length; i++) {
+            _addPayee(_payees[i] , _shares[i]);
+        }
+    }
+
+    receive() external payable virtual {
+        emit PaymentReceived(msg.sender , msg.value);
+    }
+
+    function release(address payable _account) public virtual {
+        require(shares[_account] > 0 , "PaymentSplitter: no payees");
+        uint256 payment = releasable(_account);
+        require(payment != 0 , "PaymentSplitter: account is not due payment");
+        totalRealeased += payment;
+        released[_account] += payment;
+        _account.transfer(payment);
+        emit PaymentReceived(_account , payment);
+    }
 }

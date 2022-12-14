51665e3da2224f10ec0293af0b3dafdf64e03176
zhou <1111>
2022-12-14 19:50:58 +0800

    update Proxy合约

--- zhou/46_ProxyContract.sol
+++ zhou/46_ProxyContract.sol
@@ -2,5 +2,29 @@
 pragma solidity ^0.8.4;
 
 contract Proxy {
-    
+    address public implementation;
+
+    constructor(address implementation_) {
+        implementation = implementation_;
+    }
+
+    fallback() external payable {
+        _delegate();
+    }
+
+    function _delegate() internal {
+        assembly {
+            let _implementation := sload(0)
+            calldatacopy(0 , 0 , calldatasize())
+            let result := delegatecall(gas() , _implementation , 0 , calldatasize() , 0 , 0)
+            returndatacopy(0 , 0 , returndatasize())
+            switch result
+            case 0 {
+                revert(0 , returndatasize())
+            }
+            default {
+                return(0 , returndatasize())
+            }
+        }
+    }
 }

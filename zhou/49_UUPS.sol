d77e7f7554548cce0ec9f4ba9985b78ccd7d4f54
zhou <1111>
2022-12-15 20:38:44 +0800

    upload UUPS2合约

--- zhou/49_UUPS.sol
+++ zhou/49_UUPS.sol
@@ -6,12 +6,42 @@ contract UUPSProxy {
     address public admin;
     string public words;
 
-    construtor(address _implementation) {
+    constructor(address _implementation) {
         admin = msg.sender;
         implementation = _implementation;
     }
 
     fallback() external payable {
         (bool success , bytes memory data) = implementation.delegatecall(msg.data);
+    }
+}
+
+contract UUPS1 {
+    address public implementation;
+    address public admin;
+    string public words;
+
+    function foo() public {
+        words = "old";
+    }
+
+    function upgrade(address newImplementation) external {
+        require(msg.sender == admin);
+        implementation = newImplementation;
+    }
+}
+
+contract UUPS2 {
+    address public implementation;
+    address public admin;
+    string public words;
+
+    function foo() public {
+        words = "new";
+    }
+
+    function upgrade(address newImplementation) external {
+        require(msg.sender == admin);
+        implementation = newImplementation;
     }
 }

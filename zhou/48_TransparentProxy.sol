20f73ead5c5a969c090d91d8d0519d6d9f524750
zhou <1111>
2022-12-15 15:52:49 +0800

    upload TransparentProxy合约

--- zhou/48_TransparentProxy.sol
+++ zhou/48_TransparentProxy.sol
@@ -5,3 +5,16 @@ contract Foo {
     bytes4 public selector1 = bytes4(keccak256("burn(uint256"));
     bytes4 public selector2 = bytes4(keccak256("collate_propagate_storage(bytes16)"));
 }
+
+contract TransparentProxy {
+    address implementation;
+    address admin;
+    string public words;
+
+    constructor(address _implementation) {
+        admin = msg.sender;
+        implementation = _implementation;
+    }
+
+    
+}

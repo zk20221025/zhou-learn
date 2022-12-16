d498860087a6f4e34a26a6882e0c87c71d41a6ac
zhou <1111>
2022-12-16 19:42:58 +0800

    update Bank合约提取函数

--- zhou/S01_ReentrancyAttack.sol
+++ zhou/S01_ReentrancyAttack.sol
@@ -8,5 +8,11 @@ contract Bank {
         balanceOf[msg.sender] += msg.value;
     }
 
-    
+    function withdraw() external payable {
+        uint256 balance = balanceOf[msg.sender];
+        require(balance > 0 , "Insufficient balance");
+        (bool success, ) = msg.sender.call{value: balance}("");
+        require(success , "Failed to send Ether");
+        balanceOf[msg.sender] = 0;
+    }
 }

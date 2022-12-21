54c109c2eb1e3ed7e23e4935d5f746f980232179
zhou <1111>
2022-12-21 19:26:19 +0800

    update BadRandomness合约铸造函数

--- zhou/S07_BadRandomness.sol
+++ zhou/S07_BadRandomness.sol
@@ -3,5 +3,15 @@ pragma solidity ^0.8.4;
 import "../34_ERC721.sol";
 
 contract BadRandomness is ERC721 {
-    
+    uint256 totalSupply;
+
+    constructor() ERC721("", ""){}
+
+    function luckyMint(uint256 luckyNumber) external {
+        uint256 randomNumber = 
+        uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))) % 100;
+        require(randomNumber == luckyNumber, "Better luck next time!");
+        _mint(msg.sender, totalSupply);
+        totalSupply++;
+    }
 }

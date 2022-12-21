0ac67e4dc2bac8b334f5f0eb6d06dcc19a680a9a
zhou <1111>
2022-12-21 20:56:59 +0800

    update ContractCheck合约

--- zhou/S08_ContractCheck.sol
+++ zhou/S08_ContractCheck.sol
@@ -3,5 +3,18 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ContractCheck is ERC20 {
-    
+    constructor() ERC20("", "") {}
+
+    function isContract(address account) public view returns (bool) {
+        uint size;
+        assembly {
+            size := extcodesize(account)
+        }
+        return size > 0;
+    }
+
+    function mint() public {
+        require(!isContract(msg.sender), "Contract not allowed!");
+        _mint(msg.sender, 100);
+    }
 }

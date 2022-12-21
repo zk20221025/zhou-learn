4e070e90102ba9ac47d8e463c72b5543c65f46ef
zhou <1111>
2022-12-21 13:29:36 +0800

    upload SigReplay合约

---
+++ zhou/S06_SignatureReplay.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract SigReplay is ERC20 {
+    address public signer;
+
+    constructor() ERC20("SigReplay", "Replay") {
+        signer = msg.sender;
+    }
+
+    function badMint(address to, uint amount, bytes memory signature) public {
+        bytes32 _msgHash = toEthSignedMessageHash(getMessageHash(to, amount));
+        require(verify(_msgHash, signature), "Invalid Signer!");
+        _mint(to, amount);
+    }
+}

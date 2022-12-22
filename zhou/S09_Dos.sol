b67c0ca9e04651ac67fc353cd8f8f7e9813816b9
zhou <1036767219@qq.com>
2022-12-22 15:17:21 +0800

    upload DoSGame合约存储函数

--- zhou/S09_Dos.sol
+++ zhou/S09_Dos.sol
@@ -5,4 +5,11 @@ contract DoSGame {
     bool public refundFinished;
     mapping(address => uint256) public balanceOf;
     address[] public players;
+
+    function deposit() external payable {
+        require(!refundFinished , "Game Over");
+        require(msg.value > 0 , "Please donate ETH");
+        balanceOf[msg.sender] = msg.value;
+        players.push(msg.sender);
+    }
 }

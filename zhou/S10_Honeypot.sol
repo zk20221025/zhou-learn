39154fcfc7a8c26ad2a40e336a91d0726ed36865
zhou <1036767219@qq.com>
2022-12-22 20:10:47 +0800

    update HoneyPot合约构造函数

--- zhou/S10_Honeypot.sol
+++ zhou/S10_Honeypot.sol
@@ -5,5 +5,20 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract HoneyPot is ERC20 , Ownable {
+    address public pair;
+
+    constructor() ERC20("HoneyPot" , "Pi Xiu") {
+        address factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+        address tokenA = address(this);
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA); 
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        pair = address(uint160(uint(keccak256(abi.encodePacked(
+        hex'ff',
+        factory,
+        salt,
+        hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+        )))));
+    }
+
     
 }

08f7ee9674d053eac24627d5335c6f485616f22e
zhou <1036767219@qq.com>
2022-12-24 19:30:14 +0800

    update TimeManipulation合约铸造函数

--- zhou/S14_TimeManipulation.sol
+++ zhou/S14_TimeManipulation.sol
@@ -3,5 +3,17 @@ pragma solidity ^0.8.4;
 import "openzeppelin-contracts/token/ERC721/ERC721.sol";
 
 contract TimeManipulation is ERC721 {
-    
+    uint256 totalSupply;
+
+    constructor() ERC721("" , "") {}
+
+    function luckyMint() external returns (bool success) {
+        if(block.timestamp % 170 == 0) {
+            _mint(msg.sender , totalSupply);
+            totalSupply++;
+            success = true;
+        } else {
+            success = false;
+        }
+    }
 }

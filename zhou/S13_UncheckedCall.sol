88472c892172d95615fe42029afd1c54c7a7a163
zhou <1036767219@qq.com>
2022-12-24 13:16:19 +0800

    upload UncheckedBank合约

---
+++ zhou/S13_UncheckedCall.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract UncheckedBank {
+    mapping (address => uint256) public balanceOf;
+
+    function deposit() external payable {
+        balanceOf[msg.sender] += msg.value;
+    }
+
+    
+}

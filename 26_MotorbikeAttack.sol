aed55705d0fd28d7f76787708d9606c89dc35afb
zhou <1036767219@qq.com>
2023-02-19 15:40:00 +0800

    update Motorbike攻击合约

--- 26_MotorbikeAttack.sol
+++ 26_MotorbikeAttack.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract Attack {
-    function attack() public {
+contract Null {
+    function kill() public {
         selfdestruct(payable(msg.sender));
     }
 }

ce73f2405f2d88b52ca8eba683bb8bcca31f5f2d
zhou <1036767219@qq.com>
2023-02-23 16:26:28 +0800

    update HashForEther

--- zhou/SWC-100.sol
+++ zhou/SWC-100.sol
@@ -14,3 +14,18 @@ contract HashForEther {
 }
 
 //没有指定函数可见性类型的函数是public默认的;函数可以指定为external、public或internal、private建议有意识地决定哪种可见性类型适合某个功能。这可以大大减少合同系统的攻击面。
+
+pragma solidity ^0.4.24;
+
+contract HashForEther {
+
+    function withdrawWinnings() public {
+        // Winner if the last 8 hex characters of the address are 0.
+        require(uint32(msg.sender) == 0);
+        _sendWinnings();
+     }
+
+     function _sendWinnings() internal{
+         msg.sender.transfer(this.balance);
+     }
+}

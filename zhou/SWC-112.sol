9740526915f31c3f10ca1e2d84c25437550de99d
zhou <1036767219@qq.com>
2023-02-27 15:04:19 +0800

    upload Proxy

---
+++ zhou/SWC-112.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.24;
+
+contract Proxy {
+
+  address owner;
+
+  constructor() public {
+    owner = msg.sender;  
+  }
+
+  function forward(address callee, bytes _data) public {
+    require(callee.delegatecall(_data));
+  }
+
+}
+
+//delegatecall需确保被受信任的合约调用

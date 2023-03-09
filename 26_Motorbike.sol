d87753d51d12e3d2170e080a61f97224b2f23f68
zhou <1036767219@qq.com>
2023-03-09 13:51:52 +0800

    update Motorbike

--- 26_Motorbike.sol
+++ 26_Motorbike.sol
@@ -126,3 +126,25 @@ contract Engine is Initializable {
 //await web3.eth.sendTransaction({from:player, to:"0x292ee0532abf61e170d8364cd18df75a2d20b690", data:web3.utils.sha3("upgradeToAndCall(address,bytes)").slice(0,10) + "000000000000000000000000" + "94c035b7d7C74A05F4FC804f00aF85487E8b10E5" + "0000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000441c0e1b5"})
 
 //await web3.eth.sendTransaction({from:player, to:"0x292ee0532abf61e170d8364cd18df75a2d20b690", data:web3.utils.sha3("initialize()").slice(0,10)})
+
+
+//采用UUPS模式的优点是需要部署的代理非常少。代理充当存储层，因此实现契约中的任何状态修改通常不会对使用它的系统产生副作用，因为只有通过委托调用使用逻辑。 
+ 
+//这并不意味着如果我们不初始化实现契约，就不应该注意可能被利用的漏洞。 
+ 
+//这是UUPS模式发布几个月后真正发现的内容的略微简化版本。 
+ 
+//方法:永远不要让实现契约未初始化;) 
+ 
+//如果你对发生了什么感兴趣，请在这里阅读更多。
+
+//要使 admin 变为 player，则可以设置 maxBalance 为 player
+//而 setMaxBalance 函数需要先使当前合约账户的余额变为 0
+//合约账户余额减少的方式在 execute 中的 call，使余额减少 value
+//但这样要使 balances[player] >= value
+//而通过 deposit 增加 balances[player] 的话，合约账户余额也会同步增加
+//所以要使用 multicall 函数来使 balances[player] 增加量为合约账户余额增加量的二倍
+//但是 multicall 中检测了 selector 使 deposit 只能调用一次
+//这可以通过 multicall 中执行两个 multicall，每个 multicall 调用一次 deposit
+
+

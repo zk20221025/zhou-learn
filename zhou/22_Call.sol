4f98ef3c5215e761f6aa20f4b7ad2c05e18ce1cc
zhou <1036767219@qq.com>
2023-03-11 17:05:28 +0800

    update call

--- zhou/22_Call.sol
+++ zhou/22_Call.sol
@@ -1,6 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+contract Call {
+    // 定义Response事件，输出call返回的结果success和data
+    event Response(bool success , bytes data);
+
+    function callSetX(address payable _addr , uint256 x) public payable {
+        // call setX()，同时可以发送ETH
+        (bool success , bytes memory data) = _addr.call {value : msg.value }(
+            abi.encodeWithSignature("setX(uint256)", x));
+        
+        emit Response(success , data);//释放事件
+    }
+
+    function callGetX(address _addr) external returns(uint256) {
+        // call getX()
+        (bool success , bytes memory data) = _addr.call(
+            abi.encodeWithSignature("getX()")
+        );
+
+        emit Response(success , data);//释放事件
+        return abi.decode(data , (uint256));
+    }
+
+    function callNonExist(address _addr) external {
+        (bool success , bytes memory data) = _addr.call(
+            abi.encodeWithSignature("foo(uint256")
+        );
+
+        emit Response(success , data);
+    }
+}
+
+// 定义Response事件，输出call返回的结果success和data
+// call setX()，同时可以发送ETH,并释放事件；
+// call getX()，并释放事件，用abi.decode来解码call的返回值data，并读出数值。
+// 用call调用不存在的合约，会出发fallback；能执行成功并返回success
+//目标合约地址.call(abi.encodeWithSignature("函数签名", 逗号分隔的具体参数));
+//目标合约地址.call{value:发送数额, gas:gas数额}(二进制编码);
+
+
+
+contract Call1 {
+    event Response(bool success , bytes data);
+
+    function callSetX(address payable _addr , uint x) external payable {
+        (bool success , bytes memory data) = _addr.call{value : msg.value}(abi.encodeWithSignature("setX(uint256)" , x));
+        emit Response(success , data);
+    }
+
+    function callGetX(address _addr) external returns (uint256) {
+        (bool success , bytes memory data) = _addr.call(abi.encodeWithSignature("getX()"));
+        emit Response(success , data);
+        return abi.decode(data , (uint256));
+    }
+
+    function 
+}
+
 contract OtherContract {
     uint256 private _x = 0;
     event Log (uint amount , uint gas);
@@ -22,32 +79,3 @@ contract OtherContract {
         x = _x;
     }
 }
-
-contract Call {
-    event Response(bool success , bytes data);
-
-    function callSetX(address payable _addr , uint256 x) public payable {
-        (bool success , bytes memory data) = _addr.call {value : msg.value }(
-            abi.encodeWithSignature("setX(uint256)", x));
-        
-        emit Response(success , data);
-    }
-
-    function callGetX(address _addr) external returns(uint256) {
-        (bool success , bytes memory data) = _addr.call(
-            abi.encodeWithSignature("getX()")
-        );
-
-        emit Response(success , data);
-        return abi.decode(data , (uint256));
-    }
-
-    function callNonExist(address _addr) external {
-        (bool success , bytes memory data) = _addr.call(
-            abi.encodeWithSignature("foo(uint256")
-        );
-
-        emit Response(success , data);
-    }
-}
-

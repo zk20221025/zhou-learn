f910ee24c17f56e844d3c40932e75d2dabb4de21
zhou <1036767219@qq.com>
2023-03-12 21:25:51 +0800

    update ABIEncode

--- zhou/27_ABIEncode.sol
+++ zhou/27_ABIEncode.sol
@@ -26,4 +26,18 @@ contract ABIEncode {
     function decode(bytes memory data) public pure returns(uint dx , address daddr , string memory dname , uint[2] memory darray) {
         (dx , daddr , dname , darray) = abi.decode(data , (uint , address , string , uint[2]));
     }
+
+    function decode() public view returns (bytes memory result) {
+        result = abi.encodeWithSelector(bytes4(keccak256("string")), name );
+    } 
 }
+
+/**
+* abi.encodePacked是按顺序压缩打包
+* 其余的编码方式会是把变长的数据 会依次放在定长的数据 后面
+* 例如encode(uint256,string,uint256,string). 实际出来的数据就是 uint256 uint256 string string
+* string由 uint256 字符串长度值 + 字符串本身的数据组成
+*  "0xAA" 就是 uint256(4) + 30 78 41 41
+* 字符串编码对应的数值 遵循ASCII标准
+* 单独编码string 中的02是（uint256 字符串长度值 + 字符串本身的数据）的长度
+*/

fcee6a8ea8775f45cdb9ab443e51da99080b4413
zhou <1036767219@qq.com>
2023-03-13 10:36:32 +0800

    update Selector

--- zhou/29_Selector.sol
+++ zhou/29_Selector.sol
@@ -1,21 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-contract Selector {
 
+// 0xd018db3e0000000000000000000000002c44b726adf1963ca47af88b284c06f30380fc78
+//前4个字节为函数选择器selector：0xd018db3e
+//后面32个字节为输入的参数：0x0000000000000000000000002c44b726adf1963ca47af88b284c06f30380fc78
+//
+contract Selector {
+    // event 返回msg.data
     event Log(bytes data);
 
-    function mint(address) external {
+    function attack(address) external {
         emit Log(msg.data);
     }
-
+// 当selector与method id相匹配时,即表示调用该函数
     function mintSelector() external pure returns (bytes4 mSelector) {
-        return bytes4(keccak256("transfer(address,uint256)"));
+        return bytes4(keccak256("attack(address)"));
     }
 
     function callWithSignature() external returns (bool , bytes memory) {
-        (bool success , bytes memory data) = address(this).call(abi.encodeWithSelector(0x6a627842, "0x2c44b726ADF1963cA47Af88B284C06f30380fC78"));
+        (bool success , bytes memory data) = address(this).call(abi.encodeWithSelector(0xd018db3e, "0x2c44b726ADF1963cA47Af88B284C06f30380fC78"));
         return(success , data);
     }
 }
-
 
